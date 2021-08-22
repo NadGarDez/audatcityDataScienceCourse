cacee7738302ca1c85cc0aad579c654f2e8fe1cf
NadGarDez <iranad@hotmail.com>
2021-08-22 11:20:15 -0400

    first audatiy's intro to data sciense course

---
+++ main.py
@@ -0,0 +1,39 @@
+#seccion de files
+
+"""
+from files import open1
+from files import open2
+from files import open3
+from files import write1
+from files import write2
+from files import renamef
+from files import delete
+
+file = "./files/hola.txt"
+file_new = "./files/nuevo.txt"
+#open1(file)
+#open2(file)h
+#open3(file)
+#write1(file_new)
+#write2(file_new)
+#renamef(file)
+delete(file)
+"""
+
+#Seccion de pandas
+"""
+from pan import superframe
+from pan import excel
+from pan import from_excel
+
+#print(superframe())
+#excel(superframe())
+#table = from_excel("./files/to_ecxelTable.xlsx")
+table = superframe()
+print(table.describe())
+print(table.dtypes)
+"""
+from firstExerciseCourse import avg_medal_count
+
+
+avg_medal_count()
